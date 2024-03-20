f328043b7719574e825d5cb802c3d4c38f8177ba
Jonathan Díaz <77712004+Jonialen@users.noreply.github.com>
2024-03-20 11:40:10 -0600

    a

--- Fase1/Factorial.lisp
+++ Fase1/Factorial.lisp
@@ -1,8 +1,10 @@
-(
-    defun factorial (num)
-        (if (= num 0) 
-        (* num (factorial (- num )))
-        )
-)
+(setq x 5)
+(defun factorial (n)
+  (if (= n 0) ; Si n es igual a 0
+      1        ; Devuelve 1 (caso base)
+      (* n (factorial (- n 1))))) ; De lo contrario, multiplica n por el factorial de n-1
 
-(print (factorial ))
+
+
+
+(print (factorial 5))
